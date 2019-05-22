20c71cdc1ee8977f0c07b3e77ee78f9b1474206e
steffanradojevic <steffanradojevic@users.noreply.github.com>
2019-05-22 21:24:18 +0200

    Memory_Page changes
    Verkrijgt de naam / id uit vorige activity en slaat ze op in string string titleDB en int idDB.

--- ProjectB/app/src/main/java/com/example/project_b/Memory_Page.java
+++ ProjectB/app/src/main/java/com/example/project_b/Memory_Page.java
@@ -1,13 +1,41 @@
 package com.example.project_b;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
+
+
 
 public class Memory_Page extends AppCompatActivity {
+
+    DatabaseHelper myDB;
+
+    private String titleDB;
+    private int idDB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory__page);
+
+        myDB = new DatabaseHelper(this);
+
+
+        Intent receivedIntent = getIntent();
+
+
+        //Verkrijgt ID
+        idDB = receivedIntent.getIntExtra("id",-1); //NOTE: -1 is just the default value
+
+        //Verkrijgt Name
+        titleDB = receivedIntent.getStringExtra("name");
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(titleDB);
     }
+
+
 }
