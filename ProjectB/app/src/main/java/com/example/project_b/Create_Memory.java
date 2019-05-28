7a1fc4dbcaf143afef63431d5ebaeb960a6daa3a
steffanradojevic <steffanradojevic@users.noreply.github.com>
2019-05-28 20:18:09 +0200

    Added Story
    Added story. Create Memory excist out of: Title, Story, Picture and location now.
    Added a database fot the story named addStory(String story, int ID). Made the xml edittext and in create memory the possibility to add the story.
    
    Still to be done: showcase the story in Memory_Page.

--- ProjectB/app/src/main/java/com/example/project_b/Create_Memory.java
+++ ProjectB/app/src/main/java/com/example/project_b/Create_Memory.java
@@ -33,7 +33,7 @@ public class Create_Memory extends AppCompatActivity {
 
     DatabaseHelper myDB;
     Button btnAdd,btnView;
-    EditText editText;
+    EditText editText, editStory;
 
     ImageView imgTakenPic;
     private static final int CAM_REQUEST = 1313;
@@ -52,6 +52,7 @@ public class Create_Memory extends AppCompatActivity {
         //imgTakenPic.setOnClickListener(new btnTakePhotoClicker());
 
         editText = (EditText) findViewById(R.id.editText);
+        editStory = (EditText) findViewById(R.id.editStory);
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnView = (Button) findViewById(R.id.btnView);
 
@@ -68,11 +69,17 @@ public class Create_Memory extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String newEntry = editText.getText().toString();
+                String newStory = editStory.getText().toString();
                 if (editText.length() != 0){
-                    AddData(newEntry);
+                    if (editStory.length() != 0){
+                        AddData(newEntry, newStory);
+                    }
+                    else {
+                        Toast.makeText(Create_Memory.this, "You must give the memory a Story.",Toast.LENGTH_LONG).show();
+                    }
                 }
                 else {
-                    Toast.makeText(Create_Memory.this, "You must put something in the text field!",Toast.LENGTH_LONG).show();
+                    Toast.makeText(Create_Memory.this, "You must give the memory a Title.",Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -137,7 +144,7 @@ public class Create_Memory extends AppCompatActivity {
         }
     }
 
-    public void AddData(String newEntry) {
+    public void AddData(String newEntry, String newText) {
         if(marker != null) {
 
             int ID = myDB.addMemory(newEntry);
@@ -148,6 +155,7 @@ public class Create_Memory extends AppCompatActivity {
             } else {
 
                 myDB.addLocation(marker.getPosition().latitude, marker.getPosition().longitude, ID);
+                myDB.addStory(newText, ID);
 
                 Log.i("Adding", newEntry + " " + marker.getPosition().latitude + " " + marker.getPosition().longitude);
 
