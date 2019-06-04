8bb90fabfd24171ffa79ac2abb7b62519e9748cf
Hubert Do <huydo@Huberts-MacBook-Pro.local>
2019-06-04 22:03:40 +0200

    Removed unused imports
    unused imports used in settings removed.

--- ProjectB/app/src/main/java/com/example/project_b/Settings.java
+++ ProjectB/app/src/main/java/com/example/project_b/Settings.java
@@ -1,22 +1,14 @@
 package com.example.project_b;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class Settings extends AppCompatActivity {
 
@@ -24,7 +16,6 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        DatabaseHelper myDB;
 
         setContentView(R.layout.activity_settings);
 
